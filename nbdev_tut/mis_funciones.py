21b21bea150b1e93959b54a7ef6d5debe3d072e8
sebastianf101 <sebastianf101@gmail.com>
2023-01-04 16:58:21 -0300

    hay que correr nbdev_prepare antes

--- nbdev_tut/mis_funciones.py
+++ nbdev_tut/mis_funciones.py
@@ -3,10 +3,10 @@
 # %% auto 0
 __all__ = ['foo', 'say_hello']
 
-# %% ../nbs/01_ejedic.ipynb 4
+# %% ../nbs/01_ejedic.ipynb 5
 def foo(): pass
 
-# %% ../nbs/01_ejedic.ipynb 5
+# %% ../nbs/01_ejedic.ipynb 6
 def say_hello(to):
     "Saludar al parámetro"
     return f'Hola querido {to}!'
